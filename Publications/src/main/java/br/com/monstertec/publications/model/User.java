0a544f84ec83cb2a31be89df2d7f3bfcd0f9c0e3
José Alberto <alberto.alves.1441@gmail.com>
2019-07-11 22:17:18 -0300

    spring data com mysql

---
+++ Publications/src/main/java/br/com/monstertec/publications/model/User.java
@@ -0,0 +1,38 @@
+package br.com.monstertec.publications.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+	
+	@Id
+	@Column(name = "ID")
+	private Integer id;
+	
+	@Column(name = "NAME")
+	private String name;
+	
+	@Column(name = "PASSWORD")
+	private String password;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String email) {
+		this.password = email;
+	}
+}
