fc59453340ac51ffe34851abc3af79fd3900ab82
Edlane <edlaneoliveiragusmao2@gmail.com>
2019-07-31 16:31:45 -0300

    conteudo das abas quem somos nós e serviços

--- publicFront/src/app/about/about.component.ts
+++ publicFront/src/app/about/about.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { B64 } from './photo';
-import { Members } from '../Models/AboutUs';
+
 
 @Component({
   selector: 'app-about',
@@ -11,42 +10,14 @@ import { Members } from '../Models/AboutUs';
 
 
 export class AboutComponent implements OnInit {
-  merbers = [];
-  constructor() { 
-    this.merbers = this.getMembers()}
+
+  constructor() {}
 
   ngOnInit() {
   }
 
 
-  getMembers(): Members[] {
-    const membersToReturn = new Array<Members>();
-    for (let i = 0; i < 4; i++) {
-      const member: Members = {
-        memberName: this.getName(),
-        memberPost: this.getPost(),
-        photo: this.getMockImage(),
-        text: this.getText(),
-      };
-      membersToReturn.push(member);
-    }
-    return membersToReturn;
-  }
 
-  getMockImage(): string {
-    const base = new B64();
-    return base.base;
-  }
 
-  getText(): string {
-    return "descriçao da formação e especialidades";
-  }
 
-  getName(): string {
-    return "junior";
-    }
-
-  getPost(): string {
-    return "desenvolvedor";
-    }
 }
